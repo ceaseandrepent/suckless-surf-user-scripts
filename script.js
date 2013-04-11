0998cee84a792f476173d47d23e4ddc3b98b7d4d
ceaseandrepent <ceaseandrepent@gmail.com>
2013-04-11 22:01:09 +0600

    Small fix.

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-CUSTOM_CSS = "<style type='text/css'>* { background-color: #073642 !important; color: #839496 !important; } body { width: 75%; margin-left: auto; margin-right: auto; }</style>";
+var CUSTOM_CSS = "<style type='text/css'>* { background-color: #073642 !important; color: #839496 !important; } body { width: 75%; margin-left: auto; margin-right: auto; }</style>";
 
 document.addEventListener('DOMContentLoaded', function () {
 	if (location.href.split('/')[2] == 'www.vedomosti.ru') {
 		var content = document.getElementById('idmasha').innerHTML;
-		document.getElementsByTagName('html')[0].innerHTML = '';
-		document.getElementsByTagName('html')[0].innerHTML += content;
-		document.getElementsByTagName('html')[0].innerHTML += CUSTOM_CSS;
+		var body = document.getElementsByTagName('body')[0];
+		body.innerHTML = content;
+		body.innerHTML += CUSTOM_CSS;
 	}
 }, false);
 
 document.addEventListener('DOMContentLoaded', function () {
 	if (location.href.split('/')[2] == 'www.securitylab.ru') {
 		var content = $('div.main_content').html();
-		document.getElementsByTagName('body')[0].innerHTML = '';
-		document.getElementsByTagName('body')[0].innerHTML += content;
-		document.getElementsByTagName('body')[0].innerHTML += CUSTOM_CSS;
+		var body = document.getElementsByTagName('body')[0];
+		body.innerHTML = '';
+		body.innerHTML += content;
+		body.innerHTML += CUSTOM_CSS;
 	}
 }, false);
 
