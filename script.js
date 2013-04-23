2497949122236053afb38d489ef1feb8d05f2962
ceaseandrepent <ceaseandrepent@gmail.com>
2013-04-24 05:57:53 +0600

    Font-size.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var CUSTOM_CSS = "<style type='text/css'>* { background-color: #073642 !important; color: #839496 !important; } body { width: 75%; margin-left: auto; margin-right: auto; text-align: justify; } code { text-align: left; } </style>";
+var CUSTOM_CSS = "<style type='text/css'>* { background-color: #073642 !important; color: #839496 !important; } body { width: 75%; margin-left: auto; margin-right: auto; text-align: justify; } body, div, code { font-size: " + Math.floor(window.screen.availHeight / 30) + "px !important; } code { text-align: left; } </style>";
 
 document.addEventListener('DOMContentLoaded', function () {
 	var urlParts = location.href.split('/');
@@ -24,6 +24,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	} else if (urlParts[2] == 'e.mail.ru' && urlParts[3] == 'cgi-bin') {
 		$('div.rb_banner').remove();
-
 	}
 }, false);
